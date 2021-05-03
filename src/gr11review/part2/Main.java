a935078a61bf743c32324454d52718ec5a6a7626
Lucy Mao <>
2021-05-03 22:01:54 +0000

    First method done and first array done Method calling doesn't work

--- src/gr11review/part2/Main.java
+++ src/gr11review/part2/Main.java
@@ -1,2 +1,10 @@
 package gr11review.part2;
+import java.io.*;
+import static gr11review.part2.Utility;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        System.out.println(Utility.zipZap("ziiiiipzap"));
+    }
+}
 
--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -1,9 +1,43 @@
 package gr11review.part2;
 import java.io.*;
 
-public class Utility {
-
-    
-
-    
+public class Utility{
+  public static String zipZap(String str) throws IOException{
+    String finalOut = "";
+    for(int i = 0; i<(str.length()); i++){
+      if(i <= str.length()-2){
+        if((str.substring(i, i+3)).equals("zap") == true){
+          finalOut += "zp";
+          i += 2;
+        }if((str.substring(i, i+3)).equals("zip") == true){
+          finalOut += "zp";
+          i += 2;
+        }else 
+        finalOut += str.charAt(i);
+      }else
+        finalOut += str.charAt(i);
+    }
+    return finalOut;
+  }
+  public static int[] tenRun(int[] nums) throws IOException{
+    int newNum = 0;
+    for(int i = 0; i < nums.length-1; i++){
+      if(nums[i] % 10 == 0){
+        newNum = nums[i];
+        if(i < nums.length-1){
+          i++; 
+        }
+        while(nums[i] % 10 != 0){
+          nums[i] = newNum;
+          if(i < nums.length-1){
+            i++;
+          }
+        }
+        if(nums[i] % 10 == 0){
+            i--;
+          }
+      }
+    }
+    return nums;
+  } 
 }
