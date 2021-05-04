7fecfe6bc2b5ca7238bb178fbeefba81819d6fe5
lucymao-12 <lucy.mao21@ycdsbk12.ca>
2021-05-04 08:22:55 -0400

    Fixed Methods1 Found out how to call methods

--- src/gr11review/part2/Main.java
+++ src/gr11review/part2/Main.java
@@ -1,2 +1,9 @@
 package gr11review.part2;
 
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        System.out.println(Utility.zipZap("ziiiiipxxzap"));
+    }
+}
--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -1,9 +1,19 @@
 package gr11review.part2;
 import java.io.*;
 
-public class Utility {
-
-    
-
-    
+public class Utility{
+  public static String zipZap(String str){
+    String finalOut = "";
+    for(int i = 0; i<(str.length()); i++){
+      if(i <= (str.length()-3)){
+        if(str.charAt(i) == 'z' && str.charAt(i+2) == 'p'){
+          finalOut += "zp";
+          i += 2;
+        }else 
+        finalOut += str.charAt(i);
+      }else
+        finalOut += str.charAt(i);
+    }
+    return finalOut;
+  }
 }
